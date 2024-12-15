7527e0a508ca17b0252bccf0cb81c631bf23e694
Abhinav <abhinav@metakaal.com>
2024-12-15 15:04:28 +0530

    download link working

--- files/urls.py
+++ files/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import FileUploadView, FileListView
-from .views import upload_file, file_upload_success, download_file, list_uploaded_files, delete_file
+from .views import upload_file, file_upload_success, download_file, list_uploaded_files, delete_file, generate_download_link, download_file_signed
 # urlpatterns = [
 #     path('upload/', FileUploadView.as_view(), name='file-upload'),
 #     path('files/', FileListView.as_view(), name='file-list'),
@@ -8,8 +8,11 @@ from .views import upload_file, file_upload_success, download_file, list_uploade
 
 urlpatterns = [
     path('upload/', upload_file, name='upload_file'),
+    path('upload/success/', file_upload_success, name='file_upload_success'),
     path('download/<int:file_id>/', download_file, name='download_file'),
-    path('file-upload/', FileUploadView.as_view(), name='file_upload'),
     path('files/', list_uploaded_files, name='list_uploaded_files'),
     path('delete/<int:file_id>/', delete_file, name='delete_file'),
+    path('download/<int:file_id>/', download_file, name='download_file'),
+    path('generate-download-link/<int:file_id>/', generate_download_link, name='generate_download_link'),
+    path('download-signed/', download_file_signed, name='download_file_signed'),
 ]
--- files/views.py
+++ files/views.py
@@ -1,14 +1,17 @@
 import base64
 import mimetypes
+from urllib.parse import urlencode
 from django.http import Http404, HttpResponse
 from rest_framework import generics
-from .models import SharedFile
+from .models import SharedFile, CustomUser
 from .serializers import SharedFileSerializer
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 from .models import UploadedFile
+from django.core.signing import Signer, TimestampSigner, BadSignature, SignatureExpired
 from django.conf import settings
+from django.urls import reverse
 from .encryption import encrypt_file_content, decrypt_file_content, get_aes_key
 class FileUploadView(generics.CreateAPIView):
     queryset = SharedFile.objects.all()
@@ -19,7 +22,7 @@ class FileListView(generics.ListAPIView):
     serializer_class = SharedFileSerializer
 
 
-
+signer = TimestampSigner()
 def home(request):
     return HttpResponse("Welcome to the Home Page")
 
@@ -48,9 +51,35 @@ def download_file(request, file_id):
     aes_key = get_aes_key()
     decrypted_content = decrypt_file_content(uploaded_file.encrypted_content, aes_key)
     encoded_content = base64.b64encode(decrypted_content).decode('utf-8')
-    
+
     return JsonResponse({'file_name': uploaded_file.file_name, 'content': encoded_content})
 
+def generate_download_link(request, file_id):
+    try:
+        uploaded_file = UploadedFile.objects.get(id=file_id)
+    except UploadedFile.DoesNotExist:
+        raise Http404("File does not exist")
+
+    signed_value = signer.sign(file_id)
+    download_url = request.build_absolute_uri(reverse('download_file_signed') + '?' + urlencode({'file_id': signed_value}))
+    return JsonResponse({'download_url': download_url})
+
+def download_file_signed(request):
+    signed_file_id = request.GET.get('file_id')
+    try:
+        file_id = signer.unsign(signed_file_id, max_age=3600)
+        uploaded_file = UploadedFile.objects.get(id=file_id)
+    except (BadSignature, SignatureExpired):
+        return HttpResponse('Invalid or expired download link', status=400)
+    except UploadedFile.DoesNotExist:
+        raise Http404("File does not exist")
+
+    aes_key = get_aes_key()
+    decrypted_content = decrypt_file_content(uploaded_file.encrypted_content, aes_key)
+    mime_type, _ = mimetypes.guess_type(uploaded_file.file_name)
+    response = HttpResponse(decrypted_content, content_type=mime_type or 'application/octet-stream')
+    response['Content-Disposition'] = f'attachment; filename="{uploaded_file.file_name}"'
+    return response
 def list_uploaded_files(request):
     files = UploadedFile.objects.all().values('id', 'file_name', 'uploaded_at')
     return JsonResponse(list(files), safe=False)
